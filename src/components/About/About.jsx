530ce5f90f072df0c3748d03c7f2d93398d5767a
Jinay Patel <jinaypatel0809@gmail.com>
2025-12-04 11:40:54 +0530

    second commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1IVb4rYvv6JKFTJ2LZisq4Luuf49D9Gw_/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
